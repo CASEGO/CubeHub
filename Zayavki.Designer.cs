b81bbe1a928f4302e46d07bed84d877713b8de90
CASEGO <98381212+CASEGO@users.noreply.github.com>
2022-04-13 00:04:19 +0300

    Контрольная точка 1

--- Zayavki.Designer.cs
+++ Zayavki.Designer.cs
@@ -33,16 +33,16 @@ namespace Новая_курсовая
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.aRM_KafeDataSet = new Новая_курсовая.ARM_KafeDataSet();
-            this.заявкиНаРаботуBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.заявки_на_работуTableAdapter = new Новая_курсовая.ARM_KafeDataSetTableAdapters.Заявки_на_работуTableAdapter();
             this.кодDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.фИОDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.должностьDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -50,16 +50,19 @@ namespace Новая_курсовая
             this.полDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.датаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.телефонDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.заявкиНаРаботуBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.aRM_KafeDataSet = new Новая_курсовая.ARM_KafeDataSet();
+            this.заявки_на_работуTableAdapter = new Новая_курсовая.ARM_KafeDataSetTableAdapters.Заявки_на_работуTableAdapter();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.aRM_KafeDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.заявкиНаРаботуBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aRM_KafeDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(446, 73);
+            this.button5.Location = new System.Drawing.Point(627, 73);
             this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(143, 63);
@@ -70,7 +73,7 @@ namespace Новая_курсовая
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(446, 15);
+            this.button4.Location = new System.Drawing.Point(627, 15);
             this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(143, 54);
@@ -81,23 +84,35 @@ namespace Новая_курсовая
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.button6);
             this.groupBox2.Controls.Add(this.textBox1);
             this.groupBox2.Controls.Add(this.button3);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Location = new System.Drawing.Point(214, 15);
+            this.groupBox2.Location = new System.Drawing.Point(315, 15);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox2.Size = new System.Drawing.Size(230, 121);
+            this.groupBox2.Size = new System.Drawing.Size(308, 121);
             this.groupBox2.TabIndex = 11;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Поиск заявки";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(148, 87);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(155, 25);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "К списку";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(4, 58);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(220, 20);
+            this.textBox1.Size = new System.Drawing.Size(299, 20);
             this.textBox1.TabIndex = 3;
             // 
             // button3
@@ -105,10 +120,11 @@ namespace Новая_курсовая
             this.button3.Location = new System.Drawing.Point(4, 87);
             this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(220, 25);
+            this.button3.Size = new System.Drawing.Size(140, 25);
             this.button3.TabIndex = 2;
             this.button3.Text = "Поиск";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
@@ -122,33 +138,53 @@ namespace Новая_курсовая
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(9, 15);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(200, 121);
+            this.groupBox1.Size = new System.Drawing.Size(302, 121);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Действия с заявками";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(4, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(292, 20);
+            this.textBox2.TabIndex = 6;
+            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(4, 71);
+            this.button2.Location = new System.Drawing.Point(152, 70);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(191, 36);
+            this.button2.Size = new System.Drawing.Size(144, 36);
             this.button2.TabIndex = 1;
             this.button2.Text = "Отклонить";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Код заявки";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(4, 30);
+            this.button1.Location = new System.Drawing.Point(4, 70);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(191, 36);
+            this.button1.Size = new System.Drawing.Size(144, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "Принять";
             this.button1.UseVisualStyleBackColor = true;
@@ -172,22 +208,8 @@ namespace Новая_курсовая
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(582, 215);
+            this.dataGridView1.Size = new System.Drawing.Size(761, 215);
             this.dataGridView1.TabIndex = 8;
-            // 
-            // aRM_KafeDataSet
-            // 
-            this.aRM_KafeDataSet.DataSetName = "ARM_KafeDataSet";
-            this.aRM_KafeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // заявкиНаРаботуBindingSource
-            // 
-            this.заявкиНаРаботуBindingSource.DataMember = "Заявки на работу";
-            this.заявкиНаРаботуBindingSource.DataSource = this.aRM_KafeDataSet;
-            // 
-            // заявки_на_работуTableAdapter
-            // 
-            this.заявки_на_работуTableAdapter.ClearBeforeFill = true;
             // 
             // кодDataGridViewTextBoxColumn
             // 
@@ -231,11 +253,25 @@ namespace Новая_курсовая
             this.телефонDataGridViewTextBoxColumn.HeaderText = "Телефон";
             this.телефонDataGridViewTextBoxColumn.Name = "телефонDataGridViewTextBoxColumn";
             // 
+            // заявкиНаРаботуBindingSource
+            // 
+            this.заявкиНаРаботуBindingSource.DataMember = "Заявки на работу";
+            this.заявкиНаРаботуBindingSource.DataSource = this.aRM_KafeDataSet;
+            // 
+            // aRM_KafeDataSet
+            // 
+            this.aRM_KafeDataSet.DataSetName = "ARM_KafeDataSet";
+            this.aRM_KafeDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // заявки_на_работуTableAdapter
+            // 
+            this.заявки_на_работуTableAdapter.ClearBeforeFill = true;
+            // 
             // Zayavki
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(782, 366);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.groupBox2);
@@ -248,9 +284,10 @@ namespace Новая_курсовая
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.aRM_KafeDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.заявкиНаРаботуBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aRM_KafeDataSet)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -277,5 +314,8 @@ namespace Новая_курсовая
         private System.Windows.Forms.DataGridViewTextBoxColumn полDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn датаDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn телефонDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Zayavki.cs
+++ Zayavki.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Новая_курсовая
 {
@@ -27,9 +28,13 @@ namespace Новая_курсовая
     //⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿
     public partial class Zayavki : Form
     {
+        public static string connectString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = ARM_Kafe.mdb"; //Создание параметров подключения к базе данных
+        private OleDbConnection myConnection;
         public Zayavki()
         {
             InitializeComponent();
+            myConnection = new OleDbConnection(connectString);
+            myConnection.Open();
         }
 
         private void button6_Click(object sender, EventArgs e)
@@ -64,7 +69,47 @@ namespace Новая_курсовая
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            int kod = Convert.ToInt32(textBox2.Text);
+            //String staff = "INSERT INTO Заказы ([Код заказа],[Номер заказа],[ФИО],[Дата доставки заказа],[Адрес заказа],[Номер телефона],[Блюдо],[Напиток],[Доставщик]) VALUES(" + kod + ",'" + NZ + "','" + Name + "', '" + Time + "','" + Adres + "','" + Phone + "', '" + dish + "','" + drink + "','" + courier + "')";
+            //INSERT INTO таблица2(ID, NAME) SELECT t1.ID, t1.NAME FROM таблица1 t1 LEFT JOIN таблица2 t2ON t1.ID = t2.IDWHERE t2.ID IS NULL
+            string query = "DELETE FROM [Заявки на работу] WHERE [Код] = " + kod;
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Данные обновлены!");
+            this.заявки_на_работуTableAdapter.Fill(this.aRM_KafeDataSet.Заявки_на_работу);
+            textBox2.Clear();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string Status = textBox1.Text;
+            string query = "SELECT [Код], ФИО, Должность, Стаж, Пол, Дата, Телефон FROM [Заявки на работу] WHERE Должность LIKE '%" + Status + "%' ";
+            OleDbDataAdapter command = new OleDbDataAdapter(query, myConnection);
+            DataTable dt = new DataTable();
+            command.Fill(dt);
+            dataGridView1.DataSource = dt;
+            myConnection.Close();
+            textBox1.Clear();
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            myConnection = new OleDbConnection(connectString);
+            myConnection.Open();
+            dataGridView1.DataSource = заявкиНаРаботуBindingSource;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int kod = Convert.ToInt32(textBox2.Text);
+            string query = "DELETE FROM [Заявки на работу] WHERE [Код] = " + kod;
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Данные обновлены!");
+            this.заявки_на_работуTableAdapter.Fill(this.aRM_KafeDataSet.Заявки_на_работу);
+            textBox2.Clear();
+        }
+
+   
     }
 }
