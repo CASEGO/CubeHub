38ba0f2776791519fb72dfb5b3ab0c1cc2b82c81
CASEGO <98381212+CASEGO@users.noreply.github.com>
2022-04-19 18:01:48 +0300

    готово

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -29,55 +29,55 @@ namespace Новая_курсовая
         /// </summary>
         private void InitializeComponent()
         {
-            this.by_SenseDelivery = new System.Windows.Forms.Button();
-            this.by_SenseOne = new System.Windows.Forms.Button();
-            this.By_KEFFIR = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // by_SenseDelivery
+            // button3
             // 
-            this.by_SenseDelivery.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.by_SenseDelivery.Location = new System.Drawing.Point(11, 72);
-            this.by_SenseDelivery.Margin = new System.Windows.Forms.Padding(2);
-            this.by_SenseDelivery.Name = "by_SenseDelivery";
-            this.by_SenseDelivery.Size = new System.Drawing.Size(249, 43);
-            this.by_SenseDelivery.TabIndex = 7;
-            this.by_SenseDelivery.Text = "Закрыть";
-            this.by_SenseDelivery.UseVisualStyleBackColor = true;
-            this.by_SenseDelivery.Click += new System.EventHandler(this.button4_Click);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button3.Location = new System.Drawing.Point(11, 72);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(249, 43);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Закрыть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button4_Click);
             // 
-            // by_SenseOne
+            // button2
             // 
-            this.by_SenseOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.by_SenseOne.Location = new System.Drawing.Point(138, 11);
-            this.by_SenseOne.Margin = new System.Windows.Forms.Padding(2);
-            this.by_SenseOne.Name = "by_SenseOne";
-            this.by_SenseOne.Size = new System.Drawing.Size(122, 57);
-            this.by_SenseOne.TabIndex = 5;
-            this.by_SenseOne.Text = "Сотрудники";
-            this.by_SenseOne.UseVisualStyleBackColor = true;
-            this.by_SenseOne.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button2.Location = new System.Drawing.Point(138, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 57);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сотрудники";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // By_KEFFIR
+            // button1
             // 
-            this.By_KEFFIR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.By_KEFFIR.Location = new System.Drawing.Point(11, 11);
-            this.By_KEFFIR.Margin = new System.Windows.Forms.Padding(2);
-            this.By_KEFFIR.Name = "By_KEFFIR";
-            this.By_KEFFIR.Size = new System.Drawing.Size(122, 57);
-            this.By_KEFFIR.TabIndex = 4;
-            this.By_KEFFIR.Text = "Меню";
-            this.By_KEFFIR.UseVisualStyleBackColor = true;
-            this.By_KEFFIR.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.button1.Location = new System.Drawing.Point(11, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 57);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Меню";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(271, 130);
-            this.Controls.Add(this.by_SenseDelivery);
-            this.Controls.Add(this.by_SenseOne);
-            this.Controls.Add(this.By_KEFFIR);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Home";
             this.Text = "Главное меню";
@@ -88,9 +88,9 @@ namespace Новая_курсовая
 
         #endregion
 
-        private System.Windows.Forms.Button by_SenseDelivery;
-        private System.Windows.Forms.Button by_SenseOne;
-        private System.Windows.Forms.Button By_KEFFIR;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
